d0fc2a986b45771013b8f259343c94ecd8d5037d
Mohamed Amer <m.amer_abutlabe@hotmail.com>
2022-05-30 10:08:56 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import Article from './Article';
+import data from './data';
+
+const getStorageTheme = () => {
+  let theme = "light-theme";
+  if (localStorage.getItem("theme")) {
+    theme = localStorage.getItem("theme")
+  }
+  return theme;
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [theme, setTheme] = useState(getStorageTheme());
+
+	const toggler = () => {
+		if (theme === 'light-theme') {
+			setTheme('dark-theme');
+		} else {
+			setTheme('light-theme');
+		}
+	};
+
+	useEffect(() => {
+    document.documentElement.className = theme;
+    localStorage.setItem("theme", theme);
+	}, [theme]);
+
+	return (
+		<main>
+			<nav>
+				<div className='nav-center'>
+					<h1>Overreacted</h1>
+					<button className='btn' onClick={toggler}>
+						toggle
+					</button>
+				</div>
+			</nav>
+			<section className='articles'>
+				{data.map((item) => {
+					return <Article key={item.id} {...item} />;
+				})}
+			</section>
+		</main>
+	);
 }
 
 export default App;
